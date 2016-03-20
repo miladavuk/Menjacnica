09bb07bd4fa7527638526b1720d4a5a8946f4891
miladavuk <milada.vukovic@gmail.com>
2016-03-20 15:28:26 +0100

    U klasu Kurs dodate metode hashCode, equals i toString. Metoda equals modifikovana da poredi kurs po datumu, skracenom nazivu valute i vrsti.

--- Menjacnica/src/kurs/Kurs.java
+++ Menjacnica/src/kurs/Kurs.java
@@ -41,6 +41,45 @@ public class Kurs {
 	public void setDatum(GregorianCalendar datum) {
 		this.datum = datum;
 	}
-	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((datum == null) ? 0 : datum.hashCode());
+		result = prime * result + ((skrNazivValute == null) ? 0 : skrNazivValute.hashCode());
+		result = prime * result + ((vrsta == null) ? 0 : vrsta.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Kurs other = (Kurs) obj;
+		if (datum == null) {
+			if (other.datum != null)
+				return false;
+		} else if (!datum.equals(other.datum))
+			return false;
+		if (skrNazivValute == null) {
+			if (other.skrNazivValute != null)
+				return false;
+		} else if (!skrNazivValute.equals(other.skrNazivValute))
+			return false;
+		if (vrsta == null) {
+			if (other.vrsta != null)
+				return false;
+		} else if (!vrsta.equals(other.vrsta))
+			return false;
+		return true;
+	}
+	@Override
+	public String toString() {
+		return "Kurs [nazivValute=" + nazivValute + ", skrNazivValute=" + skrNazivValute + ", vrsta=" + vrsta
+				+ ", iznos=" + iznos + ", datum=" + datum + "]";
+	}
 	
 }
