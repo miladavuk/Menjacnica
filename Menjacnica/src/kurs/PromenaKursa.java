d53ab44c401072d7f4bd40dec8c34275d6ce3877
miladavuk <milada.vukovic@gmail.com>
2016-03-20 15:40:25 +0100

    U paketu kurs napravljena klasa PromenaKursa koja implementira interfejs KursInterfejs. Metode klase su prazne.

---
+++ Menjacnica/src/kurs/PromenaKursa.java
@@ -0,0 +1,25 @@
+package kurs;
+
+import java.util.GregorianCalendar;
+
+import interfejsi.KursInterfejs;
+
+public class PromenaKursa implements KursInterfejs {	
+
+	@Override
+	public boolean dodajKurs(String nazivValute, String skrNazivValute, String vrsta, double iznos,
+			GregorianCalendar datum) {				
+		return false;
+	}
+
+	@Override
+	public boolean obrisiKurs(String skrNazivValute, String vrsta, GregorianCalendar datum) {		
+		return false;
+	}
+
+	@Override
+	public Kurs vratiKurs(String skrNazivValute, String vrsta, GregorianCalendar datum) {		
+		return null;
+	}
+
+}
